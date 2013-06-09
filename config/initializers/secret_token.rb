1852cf0d29eb86734459dd714e23014435a510ea
nzw3yl <roger.smith@ceryx.com>
2013-06-08 20:56:02 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heatcheck::Application.config.secret_token = '3c6b8ed13e0c05bd43de97ad72bbd4bb2d3cbd1ba465a0b4ae9f85ba4a8570b0d829e38d9f6b25a85ebbd20ce765afc0805e677688a6fb39aac3417862358a4c'
